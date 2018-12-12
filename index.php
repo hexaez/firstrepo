82d196820984fc43d80fee4046135a25cbb26523
Aaron <hexaez22@gmail.com>
2018-12-12 11:34:17 +0800

    changed from html to php and added readme

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First GitHub Repo</title>
+</head>
+<body>
+<h1>First GitHub Repo</h1>
+<form>
+	<input type="text" name="username" required>
+	<input type="password" name="password" required>
+	<input type="submit" name="submit" value="Submit">
+</form>
+</body>
+</html>
